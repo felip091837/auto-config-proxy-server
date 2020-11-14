cf05057c10e099443d9fcdde120278ba5b58b349
Felipe <felipesampaio@pm.me>
2020-11-14 10:52:29 +0000

    Update auto_squid.sh

--- auto_squid.sh
+++ auto_squid.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 
 #felipesi - 2017
+#tested on ubuntu 20.04 LTS aws
+
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root"
+	exit
+fi
 
 echo "UPDATING SYSTEM, WAIT..."
-apt-get update -y > /dev/null
-apt-get upgrade -y > /dev/null
+apt update -y &>/dev/null
+apt upgrade -y &>/dev/null
 echo -e "UPDATE SYSTEM - [OK]\n"
 
 echo "INSTALLING SERVER PROXY SQUID..."
-apt-get install squid3 -y > /dev/null
+apt install squid -y &>/dev/null
 echo -e "INSTALL SQUID - [OK]\n"
 
 echo "INSTALLING CURL..."
-apt-get install curl -y > /dev/null
+apt install curl -y &>/dev/null
 echo -e "INSTALL CURL - [OK]\n"
 
-cd /etc/squid3
-
-ip=$(curl -s icanhazip.com) > /dev/null
+ip=$(curl -s icanhazip.com) &>/dev/null
 
 echo "SETTING UP SQUID..."
 
-echo "http_port 8080" > squid.conf
-echo "acl net dstdomain $ip" >> squid.conf
-echo "acl REQUEST method GET" >> squid.conf
-echo "acl REQUEST method POST" >> squid.conf
-echo "acl REQUEST method OPTIONS" >> squid.conf
-echo "acl REQUEST method HEAD" >> squid.conf
-echo "acl REQUEST method CONNECT" >> squid.conf
-echo "http_access allow net" >> squid.conf
-echo "http_access allow REQUEST" >> squid.conf
-echo "http_access deny all" >> squid.conf
-echo "acl liberado url_regex -i" >> squid.conf
+cat <<EOT > /etc/squid/squid.conf
+	http_port 8080
+	acl net dstdomain
+	acl REQUEST method GET
+	acl REQUEST method POST
+	acl REQUEST method OPTIONS
+	acl REQUEST method HEAD
+	acl REQUEST method CONNECT
+	http_access allow net
+	http_access allow REQUEST
+	http_access deny all
+	acl liberado url_regex -i
+EOT
 
 echo -e "SETTING UP SQUID - [OK]\n"
 
 echo "RESTARTING SERVICES..."
-service squid3 restart
+systemctl restart squid.service
 echo -e "RESTART SERVICES - [OK]\n"
 
 echo "YOUR PROXY SERVER IS READY - IP: $ip PORT: 8080"
